a0e66d59017b0094e166d7862ee68414d4a20e13
Benedek Farkas <benedek.farkas@lombiq.com>
2021-05-18 22:58:58 +0200

    Code styling/quality improvements in Add-AzureWebAppSqlDatabaseContainedUser and Invoke-AzureWebAppSqlQuery

--- PowerShell/Azure/Sql/Add-AzureWebAppSqlDatabaseContainedUser/Add-AzureWebAppSqlDatabaseContainedUser.psm1
+++ PowerShell/Azure/Sql/Add-AzureWebAppSqlDatabaseContainedUser/Add-AzureWebAppSqlDatabaseContainedUser.psm1
@@ -1,12 +1,17 @@
 <#
 .Synopsis
-   Adds a contained user to a SQL Azure database.
+    Adds a contained user to a SQL Azure database with the optionally specified role.
 
 .DESCRIPTION
-   Adds a contained user to a SQL Azure database specified by a Subscription name, a Web App name, the database's Connection String name and a Connection String name that provides the user credentials.
+    Adds a contained user to a SQL Azure database specified by a Subscription name, a Web App name, the database's
+    Connection String name and a Connection String name that provides the user credentials.
 
 .EXAMPLE
-   Add-AzureWebAppSqlDatabaseContainedUser -ResourceGroupName "LikeAndSubscribe" -WebAppName "AppsEverywhere" -ConnectionStringName "Lombiq.Hosting.ShellManagement.ShellSettings.RootConnectionString.Localhost-master" -UserConnectionStringName "Lombiq.Hosting.ShellManagement.ShellSettings.RootConnectionString.Localhost"
+    Add-AzureWebAppSqlDatabaseContainedUser `
+        -ResourceGroupName "LikeAndSubscribe" `
+        -WebAppName "AppsEverywhere" `
+        -ConnectionStringName "Lombiq.Hosting.ShellManagement.ShellSettings.RootConnectionString.Localhost-master" `
+        -UserConnectionStringName "Lombiq.Hosting.ShellManagement.ShellSettings.RootConnectionString.Localhost"
 #>
 
 
@@ -16,17 +21,17 @@ function Add-AzureWebAppSqlDatabaseContainedUser
     [Alias("aawasdcu")]
     Param
     (
-        [Parameter(Mandatory = $true, HelpMessage = "The name of the Azure subscription on which the Web App is stored. The script throws exception if there is no subscription registered with the given name.")]
-        [string] $ResourceGroupName = $(throw "You need to provide the name of the Azure subscription."),
+        [Parameter(Mandatory = $true, HelpMessage = "You need to provide the name of the Resource Group.")]
+        [string] $ResourceGroupName,
 
-        [Parameter(Mandatory = $true, HelpMessage = "The name of the Azure Web App. The script throws exception if the Web App doesn't exist on the given subscription.")]
-        [string] $WebAppName = $(throw "You need to provide the name of the Web App."),
+        [Parameter(Mandatory = $true, HelpMessage = "You need to provide the name of the Web App.")]
+        [string] $WebAppName,
 
-        [Parameter(Mandatory = $true, HelpMessage = "The name of the connection string of the database. The script will exit with error if there is no connection string defined with the name provided for the Production slot of the given Web App.")]
-        [string] $ConnectionStringName = $(throw "You need to provide a connection string name for the database to run the query with."),
+        [Parameter(Mandatory = $true, HelpMessage = "You need to provide a connection string name for the database to run the query with.")]
+        [string] $ConnectionStringName,
 
-        [Parameter(Mandatory = $true, HelpMessage = "The name of the connection string that holds the user credentials to add to the database. The script will exit with error if there is no connection string defined with the name provided for the Production slot of the given Web App.")]
-        [string] $UserConnectionStringName = $(throw "You need to provide a connection string name for the user credentials."),
+        [Parameter(Mandatory = $true, HelpMessage = "You need to provide a connection string name for the user credentials.")]
+        [string] $UserConnectionStringName,
 
         [Parameter(HelpMessage = "The role of the user to be added to the database. The default value is `"db_owner`".")]
         [string] $UserRole = "db_owner"
@@ -39,10 +44,18 @@ function Add-AzureWebAppSqlDatabaseContainedUser
             throw ("The database and user connection string names can not be the same!")
         }
 
-        $userDatabaseConnection = Get-AzureWebAppSqlDatabaseConnection -ResourceGroupName $ResourceGroupName -WebAppName $WebAppName -ConnectionStringName $UserConnectionStringName
+        $userDatabaseConnection = Get-AzureWebAppSqlDatabaseConnection `
+            -ResourceGroupName $ResourceGroupName `
+            -WebAppName $WebAppName `
+            -ConnectionStringName $UserConnectionStringName
 
-        $query = "CREATE USER [$($userDatabaseConnection.UserName)] WITH PASSWORD = '$($userDatabaseConnection.Password)'; ALTER ROLE [$UserRole] ADD MEMBER [$($userDatabaseConnection.UserName)];"
+        $query = "CREATE USER [$($userDatabaseConnection.UserName)] WITH PASSWORD = '$($userDatabaseConnection.Password)';" +
+            "ALTER ROLE [$UserRole] ADD MEMBER [$($userDatabaseConnection.UserName)];"
 
-        return Invoke-AzureWebAppSqlQuery -ResourceGroupName $ResourceGroupName -WebAppName $WebAppName -ConnectionStringName $ConnectionStringName -Query $query
+        return Invoke-AzureWebAppSqlQuery `
+            -ResourceGroupName $ResourceGroupName `
+            -WebAppName $WebAppName `
+            -ConnectionStringName $ConnectionStringName `
+            -Query $query
     }
 }
--- PowerShell/Azure/Sql/Invoke-AzureWebAppSqlQuery/Invoke-AzureWebAppSqlQuery.psm1
+++ PowerShell/Azure/Sql/Invoke-AzureWebAppSqlQuery/Invoke-AzureWebAppSqlQuery.psm1
@@ -6,23 +6,32 @@ function Invoke-AzureWebAppSqlQuery
     [Alias()]
     Param
     (
-        [Parameter(Mandatory = $true, HelpMessage = "The name of the Azure subscription on which the Web App is stored. The script throws exception if there is no subscription registered with the given name.")]
-        [string] $ResourceGroupName = $(throw "You need to provide the name of the Azure subscription."),
+        [Parameter(Mandatory = $true, HelpMessage = "You need to provide the name of the Resource Group.")]
+        [string] $ResourceGroupName,
 
-        [Parameter(Mandatory = $true, HelpMessage = "The name of the Azure Web App. The script throws exception if the Web App doesn't exist on the given subscription.")]
-        [string] $WebAppName = $(throw "You need to provide the name of the Web App."),
+        [Parameter(Mandatory = $true, HelpMessage = "You need to provide the name of the Web App.")]
+        [string] $WebAppName,
 
-        [Parameter(Mandatory = $true, HelpMessage = "The name of a connection string. The script will exit with error if there is no connection string defined with the name provided for the Production slot of the given Web App.")]
-        [string] $ConnectionStringName = $(throw "You need to provide a connection string name."),
+        [Parameter(Mandatory = $true, HelpMessage = "You need to provide a connection string name.")]
+        [string] $ConnectionStringName,
 
-        [Parameter(Mandatory = $true)]
-        [string] $Query = $(throw "You need to define a query to run.")
+        [Parameter(Mandatory = $true, HelpMessage = "You need to define a query to run.")]
+        [string] $Query
     )
 
     Process
     {
-        $databaseConnection = Get-AzureWebAppSqlDatabaseConnection -ResourceGroupName $ResourceGroupName -WebAppName $WebAppName -ConnectionStringName $ConnectionStringName
+        $databaseConnection = Get-AzureWebAppSqlDatabaseConnection `
+            -ResourceGroupName $ResourceGroupName `
+            -WebAppName $WebAppName `
+            -ConnectionStringName $ConnectionStringName
 
-        return Invoke-Sqlcmd -ServerInstance "$($databaseConnection.ServerName).database.windows.net" -Database $databaseConnection.DatabaseName -Username $databaseConnection.UserName -Password $databaseConnection.Password -Query $Query -EncryptConnection
+        return Invoke-Sqlcmd `
+            -ServerInstance "$($databaseConnection.ServerName).database.windows.net" `
+            -Database $databaseConnection.DatabaseName `
+            -Username $databaseConnection.UserName `
+            -Password $databaseConnection.Password `
+            -Query $Query `
+            -EncryptConnection
     }
 }
