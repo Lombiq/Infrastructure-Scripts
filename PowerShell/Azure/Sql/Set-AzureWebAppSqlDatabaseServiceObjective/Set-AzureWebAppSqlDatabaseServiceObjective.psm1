6efb8620e80d50615ddb4fb9d70949589c1523ca
Benedek Farkas <benedek.farkas@lombiq.com>
2021-05-21 18:11:22 +0200

    Updating Set-AzureWebAppSqlDatabaseServiceObjective with SlotName parameter, code styling and quality improvements

--- PowerShell/Azure/Sql/Set-AzureWebAppSqlDatabaseServiceObjective/Set-AzureWebAppSqlDatabaseServiceObjective.psm1
+++ PowerShell/Azure/Sql/Set-AzureWebAppSqlDatabaseServiceObjective/Set-AzureWebAppSqlDatabaseServiceObjective.psm1
@@ -1,12 +1,18 @@
 <#
 .Synopsis
-   Changes the pricing tier of a SQL Azure database.
+    Changes the pricing tier of a SQL Azure database.
 
 .DESCRIPTION
-   Changes the pricing tier of a SQL Azure database defined by a Subscription name, the name of a Web App and the name of a Connection String.
+    Changes the pricing tier of a SQL Azure database defined by a Subscription name, the name of a Web App and the name
+    of a Connection String.
 
 .EXAMPLE
-   Set-AzureWebAppSqlDatabaseServiceObjective -ResourceGroupName "GreatStuffHere" -WebAppName "CloudFirst" -ConnectionStringName "DatabaseFirst" -ServiceObjectiveName "S2"
+    Set-AzureWebAppSqlDatabaseServiceObjective `
+        -ResourceGroupName "GreatStuffHere" `
+        -WebAppName "CloudFirst" `
+        -SlotName "MobileFirst" `
+        -ConnectionStringName "DatabaseFirst" `
+        -ServiceObjectiveName "S2"
 #>
 
 
@@ -19,61 +25,86 @@ function Set-AzureWebAppSqlDatabaseServiceObjective
     [OutputType([Microsoft.Azure.Commands.Sql.Database.Model.AzureSqlDatabaseModel])]
     Param
     (
-        [Parameter(Mandatory = $true, HelpMessage = "The name of the Resource Group the Web App is in.")]
-        [string] $ResourceGroupName = $(throw "You need to provide the name of the Resource Group."),
+        [Parameter(Mandatory = $true, HelpMessage = "You need to provide the name of the Resource Group.")]
+        [string] $ResourceGroupName,
 
-        [Parameter(Mandatory = $true, HelpMessage = "The name of the Azure Web App. The script throws exception if the Web App doesn't exist on the given subscription.")]
-        [string] $WebAppName = $(throw "You need to provide the name of the Web App."),
+        [Parameter(Mandatory = $true, HelpMessage = "You need to provide the name of the Web App.")]
+        [string] $WebAppName,
+        
+        [Parameter(HelpMessage = "The name of the Web App slot.")]
+        [string] $SlotName,
 
-        [Parameter(HelpMessage = "The name of a connection string. The script will exit with error if there is no connection string defined with the name provided for the Production slot of the given Web App.")]
-        [string] $ConnectionStringName = $(throw "You need to provide a connection string name."),
+        [Parameter(Mandatory = $true, HelpMessage = "You need to provide a connection string name.")]
+        [string] $ConnectionStringName,
 
-        [Parameter(HelpMessage = "The short name of the pricing tier to scale the database to. Examples: B, S1, S4, P4, P6")]
-        [string] $ServiceObjectiveName = $(throw "You need to provide the name of the service objective to scale the database to."),
+        [Parameter(Mandatory = $true, HelpMessage = "You need to provide the name of the service objective to scale " +
+            "the database to. Examples: B, S1, S4, P4, P6")]
+        [string] $ServiceObjectiveName,
 
-        [Parameter(Helpmessage = "The edition of the database pricing tier to scale the database to. It needs to be defined if the target Service Objective is in a different edition than the current one.")]
+        [Parameter(Helpmessage = "The edition of the database pricing tier to scale the database to. It needs to be " +
+            "defined if the target Service Objective is in a different edition than the current one.")]
         [string] $Edition = "Standard"
     )
 
     Process
     {
-        $database = Get-AzureWebAppSqlDatabase -ResourceGroupName $ResourceGroupName -WebAppName $WebAppName -ConnectionStringName $ConnectionStringName
+        $database = Get-AzureWebAppSqlDatabase `
+            -ResourceGroupName $ResourceGroupName `
+            -WebAppName $WebAppName `
+            -SlotName $SlotName `
+            -ConnectionStringName $ConnectionStringName
 
-        if ($database -eq $null)
+        if ($null -eq $database)
         {
             throw ("Database not found for the `"$ConnectionStringName`" connection string!")
         }
         elseif ($database.CurrentServiceObjectiveName -ne $database.RequestedServiceObjectiveName)
         {
-            Write-Warning ("Another operation is still pending for the database named `"$($database.DatabaseName)`" on the server `"$($database.ServerName)`"!")
+            Write-Warning ("Another operation is still pending for the database named `"$($database.DatabaseName)`" " +
+                "on the server `"$($database.ServerName)`"!")
             
             return $null
         }
         elseif ($database.CurrentServiceObjectiveName -eq $ServiceObjectiveName)
         {
-            Write-Warning ("The database named `"$($database.DatabaseName)`" on the server `"$($database.ServerName)`" is already running in the `"$ServiceObjectiveName`" tier!")
+            Write-Warning ("The database named `"$($database.DatabaseName)`" on the server `"$($database.ServerName)`" " +
+                "is already running in the `"$ServiceObjectiveName`" tier!")
 
             return $null
         }
 
-        $serviceObjectives = Get-AzSqlServerServiceObjective -ResourceGroupName $ResourceGroupName -ServerName $database.ServerName
+        $serviceObjectives = Get-AzSqlServerServiceObjective `
+            -ResourceGroupName $ResourceGroupName `
+            -ServerName $database.ServerName
 
-        $availableServiceObjectiveNames = $serviceObjectives | Where-Object { !$PSItem.IsSystem -and $PSItem.Enabled } | Select-Object -ExpandProperty "ServiceObjectiveName"
+        $availableServiceObjectiveNames = $serviceObjectives `
+        | Where-Object { !$PSItem.IsSystem -and $PSItem.Enabled } `
+        | Select-Object -ExpandProperty "ServiceObjectiveName"
 
         if (!$availableServiceObjectiveNames.Contains($ServiceObjectiveName))
         {
-            throw ("The `"$ServiceObjectiveName`" tier is not available for the server `"$($database.ServerName)`". The available tiers are:`n$([string]::Join(", ", $availableServiceObjectiveNames)).")
+            throw ("The `"$ServiceObjectiveName`" tier is not available for the server `"$($database.ServerName)`". " +
+                "The available tiers are:`n$([string]::Join(", ", $availableServiceObjectiveNames)).")
         }
 
         try
         {
-            Write-Host ("`n*****`nChanging Service Objective of the database named `"$($database.DatabaseName)`" on the server `"$($database.ServerName)`" from `"$($database.Edition) $($database.CurrentServiceObjectiveName)`" to `"$($Edition) $($ServiceObjectiveName)`"...`n*****")
+            Write-Host ("`n*****`nChanging Service Objective of the database named `"$($database.DatabaseName)`" on " +
+                "the server `"$($database.ServerName)`" from `"$($database.Edition) $($database.CurrentServiceObjectiveName)`" " +
+                "to `"$($Edition) $($ServiceObjectiveName)`"...`n*****")
 
-            return Set-AzSqlDatabase -ResourceGroupName $ResourceGroupName -ServerName $database.ServerName -DatabaseName $database.DatabaseName -RequestedServiceObjectiveName $ServiceObjectiveName -Edition $Edition -ErrorAction Stop
+            return Set-AzSqlDatabase -ResourceGroupName $ResourceGroupName `
+                -ServerName $database.ServerName `
+                -DatabaseName $database.DatabaseName `
+                -RequestedServiceObjectiveName $ServiceObjectiveName `
+                -Edition $Edition `
+                -ErrorAction Stop
         }
         catch
         {
-            Write-Error ("Changing the Service Objective failed - see the detailed error below. Did you define the Edition of the target Service Objective? It's current value is `"$Edition`".")
+            Write-Error ("Changing the Service Objective failed - see the detailed error below. Did you define the " +
+                "Edition of the target Service Objective? The current value is `"$Edition`".")
+            
             throw
         }
     }
