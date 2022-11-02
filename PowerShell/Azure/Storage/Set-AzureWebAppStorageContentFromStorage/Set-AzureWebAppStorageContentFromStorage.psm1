4fe48c1e9b96c5a703bbe322f88dff50cc77726b
Szabolcs Deme <szabolcs.deme@lombiq.com>
2022-11-02 15:42:12 +0100

    Removing debug code from another file

--- PowerShell/Azure/Storage/Set-AzureWebAppStorageContentFromStorage/Set-AzureWebAppStorageContentFromStorage.psm1
+++ PowerShell/Azure/Storage/Set-AzureWebAppStorageContentFromStorage/Set-AzureWebAppStorageContentFromStorage.psm1
@@ -118,7 +118,6 @@ function Set-AzureWebAppStorageContentFromStorage
 
     Process
     {
-        echo "1"
         $sourceStorageConnection = Get-AzureWebAppStorageConnection `
             -ResourceGroupName $SourceResourceGroupName `
             -WebAppName $SourceWebAppName `
@@ -129,7 +128,7 @@ function Set-AzureWebAppStorageContentFromStorage
             -WebAppName $DestinationWebAppName `
             -SlotName $DestinationSlotName `
             -ConnectionStringName $DestinationConnectionStringName
-        echo "2"
+
         if ($sourceStorageConnection.AccountName -eq $destinationStorageConnection.AccountName)
         {
             throw ("The destination Storage Account can not be the same as the source!")
@@ -141,7 +140,7 @@ function Set-AzureWebAppStorageContentFromStorage
         $destinationStorageContext = New-AzStorageContext `
             -StorageAccountName $destinationStorageConnection.AccountName `
             -StorageAccountKey $destinationStorageConnection.AccountKey
-        echo "2.1"
+
         $containerWhiteListValid = $ContainerWhiteList -and $ContainerWhiteList.Count -gt 0
         $containerBlackListValid = $ContainerBlackList -and $ContainerBlackList.Count -gt 0
         
@@ -151,7 +150,7 @@ function Set-AzureWebAppStorageContentFromStorage
             ((!$containerWhiteListValid -or ($containerWhiteListValid -and $ContainerWhiteList.Contains($PSItem.Name))) `
                     -and ($containerWhiteListValid -or (!$containerBlackListValid -or !$ContainerBlackList.Contains($PSItem.Name)))) `
         }
-        echo "3"
+
         # Removing containers on the destination, if necessary.
         if ($RemoveExtraFilesOnDestination)
         {
@@ -163,7 +162,7 @@ function Set-AzureWebAppStorageContentFromStorage
 
         $folderWhiteListValid = $FolderWhiteList -and $FolderWhiteList.Count -gt 0
         $folderBlackListValid = $FolderBlackList -and $FolderBlackList.Count -gt 0
-        echo "4"
+
         foreach ($sourceContainer in $sourceContainers)
         {
             $destinationContainerName = $DestinationContainerNamePrefix + $sourceContainer.Name + $DestinationContainerNameSuffix
